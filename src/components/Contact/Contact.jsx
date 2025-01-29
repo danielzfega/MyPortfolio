806f924abae02bbabcd6a6680b6428a035aca071
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-01-28 13:46:18 -1200

    Implemented Swiper JS into Projects section, and Contact section Design

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Contact.css'
+import { BsMailboxFlag } from "react-icons/bs";
+import { LuPhoneCall } from "react-icons/lu";
 
 const Contact = () => {
+  const [listContacts] = useState([
+    {
+      title: 'Phone Number',
+      value: '+234-4104-9407',
+      icon: <LuPhoneCall/>
+    },
+    {
+      title: 'Email',
+      value: 'omajeneoghenefega11@gmail.com',
+      icon: <BsMailboxFlag/> 
+    }
+  ])
   return (
-    <div>Contact</div>
+    <section className="contacts">
+      <div className="title">
+        Contact Me
+      </div>
+      <div className='contactDesc'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla delectus esse illo corrupti similique voluptates ducimus officia minus consequuntur, non vel iure vero maxime quidem praesentium alias culpa! Voluptatem, soluta.
+      </div>
+      <div className="listContact">
+        {
+          listContacts.map((value, key) =>(
+            <div key={key} className="listContactItem">
+              <h3>{value.title} <i>{value.icon}</i></h3>
+              <h4>{value.value} </h4>
+            </div>
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,30 +1,39 @@
 import React, {useState} from 'react'
-import { FaPersonCircleQuestion, FaEarthAmericas } from 'react-icons/fa6'
+import { FaPersonCircleQuestion, FaEarthAmericas, FaArrowLeft, FaArrowRight } from 'react-icons/fa6'
 import homestyler from '../../images/homestyler.png'
+import studyhub from '../../images/studyhub.png'
+import weather1 from '../../images/weather1.0.png'
 import './Projects.css'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import { Navigation, Pagination, EffectCoverflow, Autoplay } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const Projects = () => {
   const [listProjects] = useState([
     {
-      name: 'Project - Home Decor',
+      name: 'HomeStyler | Interior Home Decor',
       des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat accusantium, necessitatibus laboriosam illum atque dolore error quam expedita molestiae et aut consequuntur distinctio. In, voluptatem qui magnam numquam blanditiis assumenda?',
-      mission: 'Front-end Developer, system analysis and design',
+      mission: 'Front-end Developer, CO-ordinator, System analysis and design',
       language: 'HTML5, CSS3, ReactJs...',
       images: homestyler
     },
     {
-      name: 'Project - Home Decor',
+      name: 'StudyHub | E-Learning Website',
       des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat accusantium, necessitatibus laboriosam illum atque dolore error quam expedita molestiae et aut consequuntur distinctio. In, voluptatem qui magnam numquam blanditiis assumenda?',
-      mission: 'Front-end Developer, system analysis and design',
+      mission: 'Front-end Developer, System design',
       language: 'HTML5, CSS3, ReactJs...',
-      images: homestyler
+      images: studyhub
     },
     {
-      name: 'Project - Home Decor',
+      name: 'Weather App Demo v1.0 ',
       des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat accusantium, necessitatibus laboriosam illum atque dolore error quam expedita molestiae et aut consequuntur distinctio. In, voluptatem qui magnam numquam blanditiis assumenda?',
       mission: 'Front-end Developer, system analysis and design',
-      language: 'HTML5, CSS3, ReactJs...',
-      images: homestyler
+      language: 'HTML5, CSS3, Javascript, ReactJs...',
+      images: weather1
     }
   ])
   return (
@@ -37,42 +46,80 @@ const Projects = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, voluptatibus. Tenetur magnam quibusdam sed ullam, nemo quo voluptates? Blanditiis odio expedita perferendis neque explicabo commodi aliquam nobis. Ut, minima perspiciatis.
         </div>
         <div className="list">
-          {
-            listProjects.map((value, key) => (
-              <div key= {key} className='item'>
+          <Swiper
+            effect={'coverflow'}
+            grabCursor = {true}
+            centeredSlides = {true}
+            loop = {true}
+            slidesPerView={1}
+            autoplay={{
+              delay: 3000, // 3 seconds delay between slides
+              disableOnInteraction: false, // Keeps autoplay running even after user interaction
+            }}
+            coverflowEffect={{
+              rotate: 0,
+              stretch: 0,
+              depth: 100,
+              modifier: 2.5,
+            }}
+            pagination = {{el: '.swiperPagination', clickable: true}}
+            // navigation = {{
+            //   nextEl: 'swiperBtnNext', 
+            //   prevEl: 'swiperBtnPrev',
+            //   clickable: true,
+            // }}
+            modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
+            className='swiperContainer'
+          >
+            {
+              listProjects.map((value, key) => (
+                <SwiperSlide key={key}>
+                  <div className='item'>
 
-                  <div className="images">
-                    <img src={value.images} alt="" />
-                  </div>
-
-                  <div className="content">
-                    <h3>{value.name}</h3>
-
-                    <div className="contentDesc">{value.des}</div>
-
-                    <div className="mission">
-                       <div className='missionIcon'><FaPersonCircleQuestion/></div>
-
-                       <div>
-                        <h4>Mission</h4>
-                        <div className="missionTxt">{value.mission}</div>
-                       </div>
-
+                    <div className="images">
+                      <img src={value.images} alt="" />
                     </div>
 
-                    <div className="mission">
-                       <div className='missionIcon'><FaEarthAmericas/></div>
+                    <div className="content">
+                      <h3>{value.name}</h3>
 
-                       <div>
-                        <h4>Language</h4>
-                        <div className="missionTxt">{value.language}</div>
-                       </div>
-                       
+                      <div className="contentDesc">{value.des}</div>
+
+                      <div className="mission">
+                        <div className='missionIcon'><FaPersonCircleQuestion/></div>
+
+                        <div>
+                          <h4>Mission</h4>
+                          <div className="missionTxt">{value.mission}</div>
+                        </div>
+
+                      </div>
+
+                      <div className="mission">
+                        <div className='missionIcon'><FaEarthAmericas/></div>
+
+                        <div>
+                          <h4>Language</h4>
+                          <div className="missionTxt">{value.language}</div>
+                        </div>
+                        
+                      </div>
                     </div>
                   </div>
-              </div>
-            ))
-          }
+                </SwiperSlide>
+             ))}
+          </Swiper>
+          
+          {/* <div className="swiperController">
+            <div className="swiperBtnPrev">
+                <i><FaArrowLeft/></i>
+            </div>
+            <div className="swiperBtnNext">
+               <i><FaArrowRight/></i>
+            </div>
+            <div className="swiperPagination"></div>
+          </div> */}
+           
         </div>
       </section>
     </div>
